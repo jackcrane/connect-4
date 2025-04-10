fa18bf9a25aabe18d1c8d37ea69408741b30ad2b
Hunter Cataldo <hcataldo03@gmail.com>
2025-04-10 09:26:49 -0500

    Submitting GameModel.java

--- src/GameModel.java
+++ src/GameModel.java
@@ -1,4 +1,46 @@
 package src;
+import java.io.Serializable;
 
-public class GameModel {
+public class GameModel implements Serializable{
+    private Gameboard board;
+    private Player[] players;
+    private int currentPlayer;
+
+    public GameModel(){
+        board = new Gameboard();
+        players = new Player[]{
+            new Player("Player 1", "Red"),
+            new Player("Player 2", "Yellow")
+        };
+        currentPlayer = 0;
+    }
+
+    public boolean dropPiece(int col){
+        if(board.isValidMove(col)){
+            board.placePiece(col, new Piece(players[currentPlayer].getColor()));
+            return True;
+        }
+        return False;
+    }
+
+    public void switchPlayer(){
+        currentPlayer = 1 - currentPlayer;
+    }
+
+    public Player getCurrentPlayer(){
+        return players[currentPlayer];
+    }
+
+    public boolean checkWin(){
+        return board.checkWin();
+    }
+
+    public boolean isDraw(){
+        return board.isFull();
+    }
+
+    public void resetGame(){
+        board.reset();
+        currentPlayer = 0;
+    }
 }
